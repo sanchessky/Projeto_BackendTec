f3e169b6a56688f1c0e5d8bee004b131d0c62d3e
Thiago Sanches <thiagomlkgame@gmail.com>
2025-06-13 14:43:14 -0300

    Atualização

--- src/Interfaces/CommandsEstoque.ts
+++ src/Interfaces/CommandsEstoque.ts
@@ -1,9 +1,11 @@
 import Commands from "./Commands";
 
 export default interface CommandsEstoque<T> extends Commands<T>{
-    PesquisarQntd
-    PesquisarEstoqueMax
-    PesquisarEstoqueMin
-    
+    PesquisarQntd(quantidade:string):T[]
+    PesquisarEstoqueMax(estoquemaximo:string):T[]
+    PesquisarEstoqueMin(estoqueminimo:string):T[]
+    PesquisarEntrada(ultima_entrada:Date):T[]
+    Pesquisarsaida(ultima_saida:Date):T[]
+
 
 }
--- src/repositories/EstoqueReoisitory.ts
+++ src/repositories/EstoqueReoisitory.ts
@@ -1,3 +1,38 @@
 import Estoque from "../classes/Estoque";
 import { conexao } from "../database/Config";
-import CommandsFuncionario from "../Interfaces/CommandsFuncionario";
+import CommandsEstoque from "../Interfaces/CommandsEstoque";
+
+
+export default class EstoqueRepository implements CommandsEstoque<Estoque> {
+    PesquisarQntd(quantidade: string): Estoque[] {
+        throw new Error("Method not implemented.");
+    }
+    PesquisarEstoqueMax(estoquemaximo: string): Estoque[] {
+        throw new Error("Method not implemented.");
+    }
+    PesquisarEstoqueMin(estoqueminimo: string): Estoque[] {
+        throw new Error("Method not implemented.");
+    }
+    PesquisarEntrada(ultima_entrada: Date): Estoque[] {
+        throw new Error("Method not implemented.");
+    }
+    Pesquisarsaida(ultima_saida: Date): Estoque[] {
+        throw new Error("Method not implemented.");
+    }
+    Cadastrar(obj: Estoque): Promise<Estoque> {
+        throw new Error("Method not implemented.");
+    }
+    Listar(): Promise<Estoque[]> {
+        throw new Error("Method not implemented.");
+    }
+    Apagar(id: number): Promise<string> {
+        throw new Error("Method not implemented.");
+    }
+    Atualizar(obj: Estoque): Promise<Estoque> {
+        throw new Error("Method not implemented.");
+    }
+    PesquisarId(id: number): Promise<Estoque> {
+        throw new Error("Method not implemented.");
+    }
+   
+}
